c065c0cd2c9b9511d461b054003ec1844cff012f
Wiilver <susareywvh@gmail.com>
2026-06-15 18:52:25 -0600

    Iniciamos estructura del Snake

--- Snake.cpp
+++ Snake.cpp
@@ -46,6 +46,43 @@
     }
 #endif
 
+struct Vector{
+    int y;
+    int x;
+};
+
+struct Buffer{
+    std::string cadena;
+    int cursor;
+    const int lCeldas = 10;
+};
+
+struct Jugador{
+    Vector coor;
+    int puntuacion;
+    char direccion;
+    std::array<std::array<char, 10>,10> direcciones;
+};
+
+void iniciarArreglo(std::array<std::array<char, 10>,10> &arr, Vector &dim){
+    for(int i = 0; i < dim.y; i++) for(int j = 0; j < dim.x; j++) arr[i][j] = ' ';
+}
+
+void iniciarBuffer(std::string &buff, Vector &dim){
+    for(int i = 0; i < dim.y; i++){
+        for(int j = 0; j < dim.x; j++) buff+="\033[97m-\033[0m";
+        buff+='\n';
+    } 
+}
+
+void actualizarCursor(const Vector &coor,const Vector &dim, const int &grueso, int &cursor){
+    cursor = coor.y*dim.x*grueso+coor.x*grueso+coor.y;
+}
+
+void actualizarCadena(int cursor, std::string nuevo, std::string &cadena){
+    for(int i = 0; i < nuevo.length(); i++) cadena[cursor+i] = nuevo[i];
+}
+
 int mainLoop(){
     int tiempo, contador;
     tiempo = 1000;
@@ -65,6 +102,34 @@ int mainLoop(){
 }
 
 int main(){
-    mainLoop();
+    Buffer buffer;
+    Jugador jugador;
+    Vector dimensiones;
+    std::array<std::array<char, 10>,10> tablero;
+    std::array<std::array<char, 10>,10> manzanas;
+
+    dimensiones.x = 10;
+    dimensiones.y = 10;   
+    
+    iniciarArreglo(tablero, dimensiones);
+    iniciarArreglo(manzanas, dimensiones);
+    iniciarArreglo(jugador.direcciones, dimensiones);
+
+    jugador.coor.x = 6;
+    jugador.coor.y = 4;
+
+    jugador.direcciones[4][5] = 'd';
+    jugador.direcciones[4][4] = 'd';
+
+    iniciarBuffer(buffer.cadena, dimensiones);
+
+    actualizarCursor(jugador.coor, dimensiones, buffer.lCeldas, buffer.cursor);
+
+    actualizarCadena(buffer.cursor, "\033[92mO", buffer.cadena);
+    actualizarCadena(buffer.cursor-10, "\033[34mO", buffer.cadena);
+    actualizarCadena(buffer.cursor-20, "\033[35mO", buffer.cadena);
+
+    std::cout<<buffer.cadena;
+
     return 0;
 }
