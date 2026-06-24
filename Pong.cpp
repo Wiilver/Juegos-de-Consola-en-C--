b9d9f755cf523d81181e75188fcb4259d4a58234
Wiilver <susareywvh@gmail.com>
2026-06-23 23:41:54 -0600

    Estaba motivado pero la racha de ejercicio llama

--- Pong.cpp
+++ Pong.cpp
@@ -1,31 +1,180 @@
 #include <iostream>
-#include <array>
+#include <vector>
 #include <chrono>
 #include <ctime>
 #include <thread>
 #include <cmath>
 
-void iniciarArray(std::array<std::array<std::string, 20>,15> &arr){
-    for(int i = 0; i < 15; i++){
-        for(int j = 0; j < 20; j++){
-            //if(((j==0)||(j==19))&&((i>4)&&(i < 9))) arr[i][j] = " # ";
-            //else 
-            arr[i][j] = " - ";
-        } 
-    }
-}
+#ifdef _WIN32
+    #include <conio.h>
+    void iniciarTerminal(){}
+    void reiniciarTerminal(){}
+#else
+    #include <unistd.h>
+    #include <termios.h>
+    #include <sys/select.h>
 
-std::string buffear(std::array<std::array<std::string, 20>,15> &arr){
-    std::string cadena = "";
-    for(int i = 0; i < 15; i++){
-        for(int j = 0; j < 20; j++) cadena+=arr[i][j];
-        cadena+="\n";
-    }
-    return cadena;
-}
+    struct termios terminalOriginal;
 
+    void iniciarTerminal(){
+        struct termios nuevaTerminal;
+
+        tcgetattr(STDIN_FILENO, &terminalOriginal);
+
+        nuevaTerminal = terminalOriginal;
+
+        nuevaTerminal.c_lflag &= ~(ICANON | ECHO);
+        nuevaTerminal.c_iflag &= ~(IGNCR | ICRNL | INLCR);
+        
+        tcsetattr(STDIN_FILENO, TCSANOW, &nuevaTerminal);
+    }
+
+    void reiniciarTerminal(){
+        tcsetattr(STDIN_FILENO, TCSANOW, &terminalOriginal);
+    }
+
+    int _kbhit() {
+        struct timeval tiempo = { 0L, 0L };
+        fd_set registros;
+        FD_ZERO(&registros);
+        FD_SET(0, &registros);
+        return select(1, &registros, NULL, NULL, &tiempo);
+    }
+
+    //La vez pasada esta funcion era incluso mas rara
+    int _getch() {
+        char ch;
+        read(0, &ch, 1);
+        return ch;
+    }
+#endif
+
+struct Coor{
+    double y;
+    double x;
+};
+
+struct Vel{
+    double vTotal;
+    double vY;
+    double vX;
+};
+
+struct Caracteres{
+    std::string vertical   = " │ ";
+    std::string horizontal = "───";
+    std::string esquinaNE  = "─┐ ";
+    std::string esquinaNO  = " ┌─";
+    std::string esquinaSO  = " └─";
+    std::string esquinaSE  = "─┘ ";
+    std::string cruceC     = "─┼─";
+    std::string cruceS     = "─┴─";
+    std::string cruceE     = "─┤ ";
+    std::string cruceN     = "─┬─";
+    std::string cruceO     = " ├─";
+    std::string vacio      = "   ";
+    std::string personaje  = " # ";
+    std::string pelota     = " O ";
+};
+
+class Buffer{
+    private:
+        int alto;
+        int ancho;
+        std::string cadena = "";
+        
+        void iniciarDimensiones(){
+            if((alto<=0)||(ancho<=0)) throw "No se puede crear un buffer con estas dimensiones";
+            dimensiones.y = alto;
+            dimensiones.x = ancho;
+        }
+        
+        void iniciarVector(){
+            for(int i = 0; i < dimensiones.y+2; i++){
+                std::vector<std::string*> temp;
+                for(int j = 0; j < dimensiones.x+2; j++) temp.push_back(&caracteres.vacio);
+                celdas.push_back(temp);
+            }
+        }
+
+        void iniciarBuffer(){
+            iniciarDimensiones();
+            iniciarVector();
+        }
+
+        void actualizarCadena(){
+            cadena = "";
+            for(int i = 0; i < dimensiones.y+2; i++){
+                for(int j = 0; j < dimensiones.x+2; j++){
+                    if((i==0)&&(j==0)) cadena+=caracteres.esquinaNO;
+                    else if((i==dimensiones.y+1)&&(j==0)) cadena+=caracteres.esquinaSO;
+                    else if((i==0)&&(j==dimensiones.x+1)) cadena+=caracteres.esquinaNE;
+                    else if((i==dimensiones.y+1)&&(j==dimensiones.x+1)) cadena+=caracteres.esquinaSE;
+                    else if((i==0)||(i==dimensiones.y+1)) cadena+=caracteres.horizontal;
+                    else if((j==0)||(j==dimensiones.x+1)) cadena+=caracteres.vertical;
+                    else cadena+=*celdas[i][j];
+                }
+                cadena+='\n';
+            }
+        }
+
+    public:
+        Coor dimensiones;
+        Caracteres caracteres;
+        std::vector<std::vector<std::string*>> celdas;
+        
+        Buffer(int cAlto, int cAncho){
+            alto = cAlto;
+            ancho = cAncho;
+            iniciarBuffer();
+        }
+
+        void impresion(){
+            actualizarCadena();
+            std::cout<<"\033[H";
+            std::cout<<cadena;
+        }
+};
+
+struct Juego{
+    Coor pelota;
+    Coor jugador;
+    Coor maquina;
+
+    Vel velocidad;
+
+    int punJugador = 0;
+    int punMaquina = 0;
+
+    double teta = 3*M_PI/4;
+
+    void iniciarJuego(Buffer &buffer){
+        if((buffer.dimensiones.y<6)||(buffer.dimensiones.x<5)) throw "No puedes tener un pong con estas dimensiones";
+        pelota.y = int(buffer.dimensiones.y/2);
+        pelota.x = int(buffer.dimensiones.x/2);
+
+        jugador.y = pelota.y;
+        jugador.x = 0;
+
+        maquina.y = pelota.y;
+        maquina.x = buffer.dimensiones.y-1;
+
+        for(int i = 0; i < 5; i++){
+            buffer.celdas[jugador.y-2+i][0] = &buffer.caracteres.personaje;
+            buffer.celdas[maquina.y-2+i][maquina.x] = &buffer.caracteres.personaje;
+        }
+
+        buffer.celdas[pelota.y][pelota.x] = &buffer.caracteres.pelota;
+
+        velocidad.vTotal = 1;
+        velocidad.vY = sin(teta);
+        velocidad.vX = cos(teta);
+    }
+};
 
 int main(){
+    Buffer buffer(15, 20);
+
     std::array<std::array<std::string, 20>,15> arr;
     std::string buff;
     std::array<double, 2> pelota = {8, 10};
